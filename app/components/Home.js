c3b6fa1e09f9da5452566c8ce74268047d6a9449
Gökhan Özdemir <gn.ozdemir@gmail.com>
2016-10-19 15:26:40 +0300

    Refactor ES6 Classes to Lighter Functions
    1. if our component does NOT have a state
    2. and ONLY HAS A Render Method 
    it does not have to be class fellas

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,23 @@
 import React from'react';
 
-class Home extends React.Component {
-  render(){
-    return (
-      <h2 className="text-center">
-        Search by Github Username Above
-      </h2>
-    )
-  }
+//if our component does NOT have a state
+//and ONLY HAS A Render Method
+const Home = () => {
+  return (
+    <h2 className="text-center">
+      Search by Github Username Above
+    </h2>
+  )
 }
+//
+// class Home extends React.Component {
+//   render(){
+//     return (
+//       <h2 className="text-center">
+//         Search by Github Username Above
+//       </h2>
+//     )
+//   }
+// }
 
 export default Home;
